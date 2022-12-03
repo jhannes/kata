62c13ba3ecedf1d47993da062233ad34d35df028
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 20:56:52 +0100

    🔵🔴 Deal with hundreds

--- NumbersInWords/NumbersInWordsExt.cs
+++ NumbersInWords/NumbersInWordsExt.cs
@@ -4,7 +4,7 @@ public static class NumbersInWordsExt
     private static Dictionary<int, string> numbers = new Dictionary<int, string>() {
         { 1, "en" },
         { 123, "et hundre og tjuetre" },
-        { 456, "fire hundre og femtiseks" },
+        //{ 456, "fire hundre og femtiseks" },
         { 789, "syv hundre og åttini" },
     };
 
@@ -20,6 +20,11 @@ public static class NumbersInWordsExt
         if (n >= 1000)
         {
             return (n / 1000).ToWords() + " tusen " + (n % 1000).ToWords();
+        }
+
+        if (n >= 100)
+        {
+            return (n / 100).ToWords() + " hundre " + (n % 100).ToWords();
         }
 
         throw new Exception("Don't know what to do with " + n);
