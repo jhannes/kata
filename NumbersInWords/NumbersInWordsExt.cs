f07b07b28d40ce0c9935fa2d8fc9830dfc640d92
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 20:54:43 +0100

    🔵🔴 Deal with thousands (base case)

--- NumbersInWords/NumbersInWordsExt.cs
+++ NumbersInWords/NumbersInWordsExt.cs
@@ -4,7 +4,7 @@ public static class NumbersInWordsExt
     private static Dictionary<int, string> numbers = new Dictionary<int, string>() {
         { 1, "en" },
         { 123, "et hundre og tjuetre" },
-        { 456_789, "fire hundre og femtiseks tusen syv hundre og åttini" },
+        //{ 456_789, "fire hundre og femtiseks tusen syv hundre og åttini" },
     };
 
     public static string ToWords(this int n)
@@ -14,6 +14,11 @@ public static class NumbersInWordsExt
         if (n >= 1_000_000)
         {
             return (n / 1_000_000).ToWords() + " millioner " + (n % 1_000_000).ToWords();
+        }
+
+        if (n >= 1000)
+        {
+            return (n / 1000).ToWords() + " tusen " + (n % 1000).ToWords();
         }
 
         throw new Exception("Don't know what to do with " + n);
