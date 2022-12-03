4324442dae1a898aa08e06bc749972baed8a4bfa
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 21:23:02 +0100

    🔴 Fill in 40, 60, 70 and 90

--- NumbersInWords.Tests/NumbersInWordsTest.cs
+++ NumbersInWords.Tests/NumbersInWordsTest.cs
@@ -8,6 +8,7 @@ public class NumbersInWordsTest
     [InlineData(13_514_015, "tretten millioner fem hundre og fjorten tusen og femten")]
     [InlineData(2_010_812, "to millioner ti tusen åtte hundre og tolv")]
     [InlineData(16_017_018_019, "seksten milliarder sytten millioner atten tusen og nitten")]
+    [InlineData(40_060_070_090, "førti milliarder seksti millioner sytti tusen og nitti")]
     [InlineData(123_456_789, "et hundre og tjuetre millioner fire hundre og femtiseks tusen syv hundre og åttini")]
     [InlineData(987_654_321, "ni hundre og åttisyv millioner seks hundre og femtifire tusen tre hundre og tjueen")]
     public void ShouldReturnEnForOne(long number, string expected)
