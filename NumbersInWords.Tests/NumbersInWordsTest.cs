f7ac8b2919a16764034bce7d91ef85c504cea434
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 20:48:35 +0100

    🔴 should convert large number

--- NumbersInWords.Tests/NumbersInWordsTest.cs
+++ NumbersInWords.Tests/NumbersInWordsTest.cs
@@ -4,6 +4,7 @@ public class NumbersInWordsTest
 {
     [Theory]
     [InlineData(1, "en")]
+    [InlineData(123_456_789, "et hundre og tjuetre millioner fire hundre og femtiseks tusen syv hundre og åttini")]
     public void ShouldReturnEnForOne(int number, string expected)
     {
         Assert.Equal(expected, number.ToWords());
