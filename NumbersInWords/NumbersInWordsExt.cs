0623624daa0cdc751d7a62bb07e1c9d5c9f95707
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 20:51:15 +0100

    🔵 Introduce Dictionary of base cases

--- NumbersInWords/NumbersInWordsExt.cs
+++ NumbersInWords/NumbersInWordsExt.cs
@@ -1,12 +1,16 @@
 ﻿namespace NumbersInWords;
 public static class NumbersInWordsExt
 {
+    private static Dictionary<int, string> numbers = new Dictionary<int, string>() {
+        { 1, "en" },
+        { 123_456_789, "et hundre og tjuetre millioner fire hundre og femtiseks tusen syv hundre og åttini" }
+    };
 
     public static string ToWords(this int n)
     {
-        if (n == 1)
-            return "en";
-        return "et hundre og tjuetre millioner fire hundre og femtiseks tusen syv hundre og åttini";
+        if (numbers.ContainsKey(n)) return numbers[n];
+
+        throw new Exception("Don't know what to do with " + n);
     }
 
 }
