c13fe5b3249a5b220a9b0b6f5e12bea7a4d6188b
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 21:18:05 +0100

    🟢 fill in 5, 13, 14 and 15

--- NumbersInWords.Tests/NumbersInWordsTest.cs
+++ NumbersInWords.Tests/NumbersInWordsTest.cs
@@ -5,6 +5,7 @@ public class NumbersInWordsTest
     [Theory]
     [InlineData(1, "en")]
     [InlineData(1_001_011, "en million et tusen og elleve")]
+    [InlineData(13_514_015, "tretten millioner fem hundre og fjorten tusen og femten")]
     [InlineData(2_010_812, "to millioner ti tusen åtte hundre og tolv")]
     [InlineData(123_456_789, "et hundre og tjuetre millioner fire hundre og femtiseks tusen syv hundre og åttini")]
     [InlineData(987_654_321, "ni hundre og åttisyv millioner seks hundre og femtifire tusen tre hundre og tjueen")]
