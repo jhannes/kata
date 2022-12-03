aa57c5c17f36889cbba01b929a70dcc0ed27c460
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 14:51:40 +0100

    🔴 it shows message in norwegian

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,16 +1,24 @@
 const english = {};
+const norwegian = {};
 
 type Message = {
-    code: "generalError";
+  code: "generalError";
 };
 
 function showMessage(language: object, message: Message) {
-    return "An error has occurred";
+  return "An error has occurred";
 }
-
 
 describe("translations", () => {
   it("shows a message in english", () => {
-    expect(showMessage(english, { code: "generalError" })).toBe("An error has occurred");
+    expect(showMessage(english, { code: "generalError" })).toBe(
+      "An error has occurred"
+    );
+  });
+
+  it("shows a message in norwegian", () => {
+    expect(showMessage(norwegian, { code: "generalError" })).toBe(
+      "En feil har inntruffet"
+    );
   });
 });
