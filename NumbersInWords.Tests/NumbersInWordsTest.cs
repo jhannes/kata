b56c40de463c4d2e50155d1676e4a77ac8b9e1e2
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 21:25:25 +0100

    🔴 Fill in single billion, 30

--- NumbersInWords.Tests/NumbersInWordsTest.cs
+++ NumbersInWords.Tests/NumbersInWordsTest.cs
@@ -9,6 +9,7 @@ public class NumbersInWordsTest
     [InlineData(2_010_812, "to millioner ti tusen åtte hundre og tolv")]
     [InlineData(16_017_018_019, "seksten milliarder sytten millioner atten tusen og nitten")]
     [InlineData(40_060_070_090, "førti milliarder seksti millioner sytti tusen og nitti")]
+    [InlineData(1_030_000_000, "en milliard tretti millioner")]
     [InlineData(123_456_789, "et hundre og tjuetre millioner fire hundre og femtiseks tusen syv hundre og åttini")]
     [InlineData(987_654_321, "ni hundre og åttisyv millioner seks hundre og femtifire tusen tre hundre og tjueen")]
     public void ShouldReturnEnForOne(long number, string expected)
