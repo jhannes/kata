a9df421f1cc48988a85344194e4f3289d391a541
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 14:41:32 +0100

    🔴 show a message in english (failing test)

--- __tests__/messages.test.ts
+++ __tests__/messages.test.ts
@@ -1,3 +1,10 @@
+const english = {};
+
+function showMessage(language, message) {
+    
+}
+
+
 describe("translations", () => {
   it("shows a message in english", () => {
     expect(showMessage(english, { code: "generalError" })).toBe("An error has occurred");
