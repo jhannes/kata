a667339fb06cac57ce34dbb2156c49b94c9fab7a
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 21:07:14 +0100

    🟢 New large number (passes instantly)

--- NumbersInWords.Tests/NumbersInWordsTest.cs
+++ NumbersInWords.Tests/NumbersInWordsTest.cs
@@ -5,6 +5,7 @@ public class NumbersInWordsTest
     [Theory]
     [InlineData(1, "en")]
     [InlineData(123_456_789, "et hundre og tjuetre millioner fire hundre og femtiseks tusen syv hundre og åttini")]
+    [InlineData(987_654_321, "ni hundre og åttisyv millioner seks hundre og femtifire tusen tre hundre og tjueen")]
     public void ShouldReturnEnForOne(int number, string expected)
     {
         Assert.Equal(expected, number.ToWords());
