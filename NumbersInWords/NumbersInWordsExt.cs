db4c79a5f161830ec8316119c6cdfb060bac9059
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 21:04:10 +0100

    🔵 Deal with tens

--- NumbersInWords/NumbersInWordsExt.cs
+++ NumbersInWords/NumbersInWordsExt.cs
@@ -3,9 +3,10 @@ public static class NumbersInWordsExt
 {
     private static Dictionary<int, string> numbers = new Dictionary<int, string>() {
         { 1, "en" },
+        { 3, "tre" },
         { 4, "fire" },
         { 7, "syv" },
-        { 23, "tjuetre" },
+        { 20, "tjue" },
         { 56, "femtiseks" },
         { 89, "åttini" },
         { 100, "et hundre" },
@@ -29,6 +30,10 @@ public static class NumbersInWordsExt
         {
             if (n % 100 == 0) return (n / 100).ToWords() + " hundre";
             return (n - n % 100).ToWords() + " og " + (n % 100).ToWords();
+        }
+        if (n >= 20)
+        {
+            return numbers[(n - n % 10)] + numbers[(n % 10)];
         }
 
         throw new Exception("Don't know what to do with " + n);
