56fa33dc818a2bb4efd07de35242ce9c1fb015fe
Johannes Brodwall <johannes@brodwall.com>
2022-12-11 16:09:52 +0000

    🟢 should translate 20

--- tests/number_as_words_test.rs
+++ tests/number_as_words_test.rs
@@ -14,6 +14,7 @@ fn number_in_words(n: i32) -> String {
         1 => "en".to_string(),
         2 => "to".to_string(),
         3 => "tre".to_string(),
+        20 => "tjue".to_string(),
         _ => panic!("Unknown {}", n)
     }
 }
