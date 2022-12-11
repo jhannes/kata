d33d220cb14fe1fd47ce771473008d887162fb25
Johannes Brodwall <johannes@brodwall.com>
2022-12-11 16:08:31 +0000

    🔴 should translate 3

--- tests/number_as_words_test.rs
+++ tests/number_as_words_test.rs
@@ -1,9 +1,9 @@
 use parameterized::parameterized;
 
 #[parameterized(n = {
-    1, 2
+    1, 2, 3
 }, expected={
-    "en", "to"
+    "en", "to", "tre"
 })]
 fn test_number_in_words(n: i32, expected: &str) {
     assert_eq!(expected, number_in_words(n));
@@ -13,6 +13,7 @@ fn number_in_words(n: i32) -> String {
     match n {
         1 => "en".to_string(),
         2 => "to".to_string(),
+        2 => "tre".to_string(),
         _ => panic!("Unknown {}", n)
     }
 }
