a7917913227e019fd72cd29bf1142bc996361c7d
Johannes Brodwall <johannes@brodwall.com>
2022-12-11 15:29:52 +0000

    Touch

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     if std::env::args().count() < 2 {
-        panic!("Must provide numbers to convert to words");
+        panic!("usage: <number>...");
     }
     for argument in std::env::args().skip(1) {
         println!("{}", kata::number_in_words(argument.parse::<i64>().unwrap()));
