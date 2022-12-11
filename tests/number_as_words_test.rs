240f4b05293dac42d5392f02004920db910d32c1
Johannes Brodwall <johannes@brodwall.com>
2022-12-11 16:23:01 +0000

    🟢 should translate

--- tests/number_as_words_test.rs
+++ tests/number_as_words_test.rs
@@ -16,7 +16,11 @@ fn number_in_words(n: i32) -> String {
         Some(n) => n.to_string(),
         None => {
             if n > 100 {
-                format!("{} og {}", number_in_words(n - n%100), number_in_words(n%100))
+                if n % 100 == 0 {
+                    format!("{} hundre", number_in_words(n / 100))                
+                } else {
+                    format!("{} og {}", number_in_words(n - n%100), number_in_words(n%100))
+                }
             } else if n > 20 && n % 10 != 0 {
                 format!("{}{}", number_in_words(n - n%10), number_in_words(n%10))
             } else {
@@ -33,6 +37,8 @@ fn base_numbers(n: i32) -> Option<&'static str> {
         3 => Some("tre"),
         4 => Some("fire"),
         5 => Some("fem"),
+        6 => Some("seks"),
+        10 => Some("ti"),
         20 => Some("tjue"),
         30 => Some("tretti"),
         40 => Some("førti"),
