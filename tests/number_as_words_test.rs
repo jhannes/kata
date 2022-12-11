cfd3500c87d1984a1654cdb59e4baf13c24d638d
Johannes Brodwall <johannes@brodwall.com>
2022-12-11 16:44:39 +0000

    🔴 should translate 59_060_070_080

--- tests/number_as_words_test.rs
+++ tests/number_as_words_test.rs
@@ -4,12 +4,14 @@ use parameterized::parameterized;
     1, 2, 3, 20, 21, 34,
     100, 145, 610, 617,
     1000, 1711, 12813,
-    14015, 1_000_000, 16_018_019
+    14015, 1_000_000, 16_018_019,
+    59_060_070_080
 }, expected={
     "en", "to", "tre", "tjue", "tjueen", "trettifire",
     "et hundre", "et hundre og førtifem", "seks hundre og ti", "seks hundre og sytten",
     "et tusen", "et tusen syv hundre og elleve", "tolv tusen åtte hundre og tretten",
-    "fjorten tusen og femten", "en million", "seksten millioner atten tusen og nitten"
+    "fjorten tusen og femten", "en million", "seksten millioner atten tusen og nitten",
+    "femtini milliarder seksti millioner sytti tusen og åtti"
 })]
 fn test_number_in_words(n: i32, expected: &str) {
     assert_eq!(expected, numbers::number_in_words(n));
