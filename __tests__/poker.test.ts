72454dcfc315b6e74d2d7a5fc3f618b6584e3a60
Johannes Brodwall <johannes@brodwall.com>
2023-07-23 21:33:44 +0200

    🔴 poker hand identifies high card (failing)

--- __tests__/poker.test.ts
+++ __tests__/poker.test.ts
@@ -1,4 +1,8 @@
+
+function pokerHand(hand: string) {}
+
 describe("poker hand", () => {
     it("shows high card", () => {
+        expect(pokerHand("Kc Qd 10c 5h 7d")).toEqual("High card (King)");
     })
 })
