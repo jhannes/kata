9b2e27c4bd4ba4a279f6c29dd9e6f1520d1ce3bd
Johannes Brodwall <johannes@brodwall.com>
2023-07-23 22:04:48 +0200

    🔴 poker hand identifies straight (fails)

--- __tests__/poker.test.ts
+++ __tests__/poker.test.ts
@@ -13,4 +13,5 @@ describe("poker hand", () => {
   itIdentifies("Pair (10)", "2c 4d 10c 10h Qd");
   itIdentifies("Two pairs (10 and 4)", "2c 4d 10c 10h 4d");
   itIdentifies("Three of a kind (10)", "10c 4d 10c 10h Qd");
+  itIdentifies("Straight (10 high)", "10c 9d 8c 6h 7d");
 });
