8d29abe3898e8e7ff595c59339c9816fb4199ddd
Johannes Brodwall <johannes@brodwall.com>
2023-07-23 21:46:43 +0200

    🔴 poker hand identifies pairs of ten

--- __tests__/poker.test.ts
+++ __tests__/poker.test.ts
@@ -30,4 +30,5 @@ describe("poker hand", () => {
     itIdentifies("High card (King)", "Kc Qd 10c 5h 7d");
     itIdentifies("High card (Queen)", "2c Qd 10c 5h 7d");
     itIdentifies("High card (10)", "2c 4d 10c 5h 7d");
+    itIdentifies("Pair (10)", "2c 4d 10c 10h Qd");
 })
