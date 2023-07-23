3f3d1b5adc02a9424f023b255d10b7dc4cfe69db
Johannes Brodwall <johannes@brodwall.com>
2023-07-23 21:41:09 +0200

    🟢 poker hand identifies high card queen (wildly insufficient!)

--- __tests__/poker.test.ts
+++ __tests__/poker.test.ts
@@ -1,6 +1,19 @@
 
+const RANK_ORDER = [
+    "Ace", "King", "Queen", "Jack"
+]
+
+const RANKS: Record<string, string> = {
+    'K': "King",
+    'Q': "Queen",
+    '10': "10",
+}
+
+
 function pokerHand(hand: string) {
-    return "High card (King)"
+    const cards = hand.split(" ").map(c => RANKS[c.substring(0, 1)]);
+    const highestCard = cards.sort((a, b) => RANK_ORDER.indexOf(a) - RANK_ORDER.indexOf(b))[0];
+    return "High card (" + highestCard + ")"
 }
 
 describe("poker hand", () => {
