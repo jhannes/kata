dc5addc77352ce2210a4b8d35f4fdfaf4fee4bec
Johannes Brodwall <johannes@brodwall.com>
2023-10-09 21:58:35 +0200

    🔴🟢 roman converter converts 3 to II in roman

--- __tests__/romans.test.ts
+++ __tests__/romans.test.ts
@@ -6,11 +6,17 @@ describe("roman converter", () => {
   it("converts from 2 to II in roman", () => {
     expect(toRoman(2)).toBe("II");
   });
+
+  it("converts from 3 to III in roman", () => {
+    expect(toRoman(3)).toBe("III");
+  });
 });
 
 function toRoman(n: number) {
   let result = "";
-  if (n === 2) {
+  if (n === 3) {
+    result = "III";
+  } else if (n === 2) {
     result = "II";
   } else if (n === 1) {
     result = "I";
