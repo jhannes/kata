d192de36ee3aa80e3538ae2cd3b15663b7a03474
Johannes Brodwall <johannes@brodwall.com>
2023-10-09 22:03:12 +0200

    four and five in rapido successiono with it.each()!

--- __tests__/romans.test.ts
+++ __tests__/romans.test.ts
@@ -10,10 +10,23 @@ describe("roman converter", () => {
   it("converts from 3 to III in roman", () => {
     expect(toRoman(3)).toBe("III");
   });
+
+  it.each([
+    [4, "IV"],
+    [5, "V"],
+  ])("converts from %d to %s in roman", (n, expectedResult) => {
+    expect(toRoman(n)).toBe(expectedResult);
+  });
 });
 
 function toRoman(n: number) {
   let result = "";
+  if (n === 5) {
+    return "V";
+  }
+  if (n === 4) {
+    return "IV";
+  }
   while (n >= 1) {
     result += "I";
     n -= 1;
