05eed8a98ad45ee68dde1fc95b0b5ca12ed44bb7
Johannes Brodwall <johannes@brodwall.com>
2023-10-09 22:15:17 +0200

    Congratulations, you are now a roman!

--- __tests__/romans.test.ts
+++ __tests__/romans.test.ts
@@ -9,6 +9,9 @@ describe("roman converter", () => {
     [9, "IX"],
     [10, "X"],
     [20, "XX"],
+    [444, "CDXLIV"],
+    [999, "CMXCIX"],
+    [3888, "MMMDCCCLXXXVIII"],
   ])("converts from %d to %s in roman", (n, expectedResult) => {
     expect(toRoman(n)).toBe(expectedResult);
   });
@@ -27,13 +30,19 @@ function toRoman(n: number) {
     }
   }
 
+  convertRomanDigit(1000, "M");
+  convertRomanDigit(900, "CM");
+  convertRomanDigit(500, "D");
+  convertRomanDigit(400, "CD");
+  convertRomanDigit(100, "C");
+  convertRomanDigit(90, "XC");
+  convertRomanDigit(50, "L");
+  convertRomanDigit(40, "XL");
   convertRomanDigit(10, "X");
   convertRomanDigit(9, "IX");
   convertRomanDigit(5, "V");
   convertRomanDigit(4, "IV");
-  while (result.number >= 1) {
-    result.roman += "I";
-    result.number -= 1;
-  }
+  convertRomanDigit(1, "I");
+
   return result.roman;
 }
