17ca6ddbd8a85df81e1a083bd0a63278ef46006f
Johannes Brodwall <johannes@brodwall.com>
2023-10-11 23:55:09 +0200

    🔴 roman numeral converter converts to number %d from %s

--- roman-numerals.test.js
+++ roman-numerals.test.js
@@ -33,4 +33,12 @@ describe("roman numeral converter", () => {
   ])("converts number %d to roman %s", (number, expected) => {
     expect(toRoman(number)).toBe(expected);
   });
+
+  it.each([
+    [3888, "MMMDCCCLXXXVIII"],
+    [999, "CMXCIX"],
+    [444, "CDXLIV"],
+  ])("converts to number %d from roman %s", (number, roman) => {
+    expect(fromRoman(roman)).toBe(number);
+  });
 });
