4ce319b66854dde600776c711686ea8e0f86cfd1
Johannes Brodwall <johannes@brodwall.com>
2023-10-11 23:37:27 +0200

    🔴 roman numeral converter converts number 1 to roman I

--- roman-numerals.test.js
+++ roman-numerals.test.js
@@ -1,4 +1,8 @@
-describe("roman numerals", () => {
+function toRoman(number) {
+  return undefined;
+}
+
+describe("roman numeral converter", () => {
   it("converts number 1 to roman I", () => {
     expect(toRoman(1)).toBe("I");
   });
